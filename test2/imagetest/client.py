2d9428cbf5f5cd48f29c3fdbceb1d3123222ae25
bnh <skagus159632@naver.com>
2023-07-06 19:44:31 +0900

    first commit

---
+++ test2/imagetest/client.py
@@ -0,0 +1,20 @@
+﻿import requests
+import json
+import base64
+import cv2
+import numpy as np
+
+image_name = 'C:\\Users\\82104\\Desktop\\testFlask\\test2\\imagetest\\car5.jpg'
+ff = np.fromfile(image_name, np.uint8)
+img = cv2.imdecode(ff, cv2.IMREAD_UNCHANGED)
+cv2.imshow("test", img)
+jpg_img = cv2.imencode('.jpg', img)
+b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
+
+
+files = {
+            "img": b64_string,
+        }
+
+r = requests.post("http://127.0.0.1:5000", json=files)
+print(r.json())
---
+++ test2/test.py
@@ -0,0 +1,37 @@
+﻿from flask import Flask, jsonify, request
+from pymongo import MongoClient
+
+# db 연동
+conn = MongoClient('127.0.0.1')
+
+# db 생성
+db = conn.gps_saver
+
+# collection 생성
+collect = db.data
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def list_library():
+    name = request.args.get('Name', 'aaa')
+    lat = request.args.get('Lat', 'bbb')
+    lon = request.args.get('Lon', 'ccc')
+    time = request.args.get('Time', 'ddd')
+
+    # document 생성
+    doc = {
+    "Name": name,
+    "Lat" : lat,
+    "Lon" : lon,
+    "Time" : time
+    }
+    # document 삽입
+    collect.insert_one(doc)
+
+    return jsonify({"code": 0, "msg": "Storage completed"})
+
+
+if __name__ == '__main__':
+    app.run()
